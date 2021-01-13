6b4ced9f66a6c4c1a9d94ac799a5839bac5b8f91
Lucas Soler <hola@lucassoler.com.ar>
2021-01-12 22:16:18 -0300

    added name and results to config

--- src/lib/config.js
+++ src/lib/config.js
@@ -14,12 +14,44 @@ export const useConfig = () => {
   return useContext(ConfigContext);
 };
 
-function useProviderConfig() {
-  const [config, setConfig] = useState({ difficultyLevel: "EASY" });
+const difficultyScore = {
+  EASY: 0,
+  MEDIUM: 1,
+  HARD: 2,
+};
 
-  function setDifficultyLevel(difficultyLevel) {
-    setConfig((state) => ({ ...state, difficultyLevel }));
+function useProviderConfig() {
+  const [config, setConfig] = useState({
+    difficulty: "EASY",
+    name: "",
+    results: [],
+  });
+
+  function setDifficulty(difficulty) {
+    setConfig((state) => ({ ...state, difficulty }));
   }
 
-  return { ...config, setDifficultyLevel };
+  function setName(name) {
+    setConfig((state) => ({ ...state, name }));
+  }
+
+  // result: startTime, endTime, difficulty, gameTime, status,
+  function addResult(newResult) {
+    setConfig((state) => {
+      const { results } = state;
+      results.push(newResult);
+      results.sort((a, b) => {
+        const diffA = difficultyScore[a.difficulty];
+        const diffB = difficultyScore[b.difficulty];
+        if (diffA < diffB) return -1;
+        if (diffA > diffB) return 1;
+        if (a.gameTime < b.gameTime) return -1;
+        if (a.gameTime > b.gameTime) return 1;
+        return 0;
+      });
+      return { ...state, results };
+    });
+  }
+
+  return { ...config, setDifficulty, setName, addResult };
 }
