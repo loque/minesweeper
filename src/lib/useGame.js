64291c5c3d9d8213e9bd47210fae2afb2ff1e07c
Lucas Soler <hola@lucassoler.com.ar>
2021-01-20 10:42:24 -0300

    prevent new Game on every useGame render

--- src/lib/useGame.js
+++ src/lib/useGame.js
@@ -5,8 +5,12 @@ export const configs = [{ rows: 10, cols: 10, mines: 20 }];
 
 export function useGame(level) {
   const config = configs[level] || configs[configs.length - 1];
-  const game = useRef(new Game(config));
+  const game = useRef();
   const [_, setTick] = useState(0); // eslint-disable-line no-unused-vars
+
+  if (!game.current) {
+    game.current = new Game(config);
+  }
 
   useEffect(() => {
     const unsuscribe = game.current.subscribe((newGame) => {
@@ -15,10 +19,13 @@ export function useGame(level) {
     return unsuscribe;
   }, [game]);
 
-  const reset = useCallback(() => {
-    game.current = new Game(config);
-    setTick((t) => t + 1);
-  }, [config]);
+  const reset = useCallback(
+    function reset() {
+      game.current = new Game(config);
+      setTick((t) => t + 1);
+    },
+    [game, config]
+  );
 
   return [game.current, reset];
 }
