b0af0d6d98463d42c74b977b6b68a9a4e8f08bb5
Lucas Soler <hola@lucassoler.com.ar>
2021-01-21 21:53:47 -0300

    + added 2 levels (portrait layout) * fixed level selector in Setup * made StatusBar sticky

--- src/lib/useGame.js
+++ src/lib/useGame.js
@@ -1,10 +1,14 @@
 import { useState, useEffect, useCallback } from "react";
 import Minesweeper from "../game/Minesweeper";
 
-export const configs = [{ rows: 10, cols: 10, mines: 20 }];
+export const levels = [
+  { rows: 10, cols: 10, mines: 20 },
+  { rows: 20, cols: 10, mines: 40 },
+  { rows: 30, cols: 10, mines: 60 },
+];
 
 export default function useGame(level) {
-  const config = configs[level - 1] || configs[0];
+  const config = levels[level - 1] || levels[0];
   const [game, setGame] = useState(() => new Minesweeper(config));
   const [_, setTick] = useState(0); // eslint-disable-line no-unused-vars
 
--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -8,6 +8,7 @@ import {
   RiFlashlightFill as DifficultyIcon,
   RiPlayFill as PlayIcon,
 } from "react-icons/ri";
+import { levels } from "../lib/useGame";
 
 export default function Setup() {
   const history = useHistory();
@@ -23,12 +24,12 @@ export default function Setup() {
     config.setName(name);
   }
 
-  function selectDifficulty(level) {
-    return () => config.setDifficulty(level);
+  function selectLevel(level) {
+    return () => config.setLevel(level);
   }
 
-  function diffClassName(level) {
-    return config.difficulty === level ? "selected" : "";
+  function levelClassName(level) {
+    return config.level === level ? "selected" : "";
   }
 
   return (
@@ -57,15 +58,18 @@ export default function Setup() {
             <DifficultyIcon /> Select difficulty level
           </h3>
           <div className="section-body difficulty">
-            {/* {Object.keys(DifficultyLevel).map((level) => (
-              <button
-                key={level}
-                className={diffClassName(level)}
-                onClick={selectDifficulty(level)}
-              >
-                {level}
-              </button>
-            ))} */}
+            {levels.map((_, levelIdx) => {
+              const level = levelIdx + 1;
+              return (
+                <button
+                  key={levelIdx}
+                  className={levelClassName(level)}
+                  onClick={selectLevel(level)}
+                >
+                  {level}
+                </button>
+              );
+            })}
           </div>
         </div>
         <div className="play-button-wrapper">
